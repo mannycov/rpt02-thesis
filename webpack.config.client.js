1dfaad14d8dd848b55c982ac8d1732a11772cf83
crizzcoxx <crizzcoxx@gmail.com>
2017-12-29 12:56:16 -0800

    final push of working hot style reload

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack')
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   devtool: 'inline-source-map',
