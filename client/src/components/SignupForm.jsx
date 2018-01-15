bd0775c83b282218c935652501b55b343dfcbc91
crizzcoxx <crizzcoxx@gmail.com>
2018-01-14 17:04:09 -0800

    registration block jsx

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -2,38 +2,49 @@ import React from 'react'
 import { Button } from 'semantic-ui-react'
 
 const SignupForm = () => (
-  <div>
-    <form className="contact_form" action="" method="post" name="contact_form">
-		  <ul>
-        <li>
-          <h2>Sign Up</h2>
-          <span className="required_notification">* Denotes Required Field</span>
-        </li>
-        <li>
-          <label for="email">Email:</label>
-          <input type="text" name="email" />
-          <span className="form_hint">Proper format "name@something.com"</span>
-        </li>
-      </ul>
-    </form>
-    <form class="ui form">
-			<div class="field">
+	<div className="ui text container">
+		<form className="ui form">
+			<div className="field">
+				<label>User Name</label>
+				<input placeholder="User Name" />
+			</div>
+			<div className="field">
 				<label>First Name</label>
 				<input placeholder="First Name" />
 			</div>
-			<div class="field">
+			<div className="field">
 				<label>Last Name</label>
 				<input placeholder="Last Name" />
 			</div>
 			<div class="field">
-				<div class="ui checkbox">
+				<label>Email</label>
+				<div class="ui input">
+					<input type="text" placeholder="Email" />
+				</div>
+			</div>
+			<div class="field">
+				<label>Password</label>
+				<div class="ui input">
+					<input type="text" placeholder="Enter Your Password" />
+				</div>
+			</div>
+			<div class="field">
+				<label>Verify Password</label>
+				<div class="ui input">
+					<input type="text" placeholder="Re-Enter Your Password" />
+				</div>
+			</div>
+			<div className="field">
+				<div className="ui checkbox">
 					<input type="checkbox" class="hidden" readonly="" tabindex="0" />
 					<label>I agree to the Terms and Conditions</label>
 				</div>
 			</div>
-			<button type="submit" class="ui button" role="button">Submit</button>
-    </form>
-  </div>
-)
+			<button type="submit" class="ui button" role="button">
+				Submit
+			</button>
+		</form>
+	</div>
+);
 
 export default SignupForm
