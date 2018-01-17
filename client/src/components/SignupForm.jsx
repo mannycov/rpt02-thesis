a00875a04dcf98e592af36dc365345cfee72aa60
crizzcoxx <crizzcoxx@gmail.com>
2018-01-17 08:17:12 -0800

    Fixing Reg Form

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -36,7 +36,7 @@ const SignupForm = () => (
 			</div>
 			<div className="field">
 				<div className="ui checkbox">
-					<input type="checkbox" className="hidden" readonly="" tabindex="0" />
+					<input type="checkbox" className="hidden" readOnly="" tabIndex="0" />
 					<label>I agree to the Terms and Conditions</label>
 				</div>
 			</div>
