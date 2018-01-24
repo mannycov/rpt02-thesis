e456cbc8298272ec50c22464c3c6a48e3cfa9a73
Emanuel <manny.cov09@gmail.com>
2018-01-24 15:51:14 -0800

    fix styling on form and sidemenu

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -3,39 +3,40 @@ import { Menu, Input } from 'semantic-ui-react'
 import CompetitionsPopUp from './CompetitionsPopUp.jsx';
 
 class SideMenu extends Component {
-
   handleItemClick (name) {
     this.setState({ activeItem: name })
   }
 
   render () {
     const { activeItem } = this.state || {}
-    return <div>
-				<Menu vertical>
-					<Menu.Item>
-						<Menu.Header>Goals</Menu.Header>
+    return (
+      <div>
+        <Menu style={{ width: 290 }} vertical>
+          <Menu.Item>
+            <Menu.Header>Goals</Menu.Header>
 
-						<Menu.Menu>
-							{this.props.goals.map(goal => (
-								<Menu.Item
-									name={goal.goals_name}
-									active={activeItem === goal}
-									onClick={this.handleItemClick}
-								>
-									{goal.goals_name}
-								</Menu.Item>
-							))}
-						</Menu.Menu>
-					</Menu.Item>
-					<Menu.Item>
-						<CompetitionsPopUp />
-					</Menu.Item>
-					<Menu.Item>
-						<Menu.Header>Trophies</Menu.Header>
-					</Menu.Item>
-				</Menu>
-			</div>
-  };
+            <Menu.Menu>
+              {this.props.goals.map(goal => (
+                <Menu.Item
+                  name={goal.goals_name}
+                  active={activeItem === goal}
+                  onClick={this.handleItemClick}
+                >
+                  {goal.goals_name}
+                </Menu.Item>
+              ))}
+            </Menu.Menu>
+          </Menu.Item>
+          <Menu.Item>
+            <CompetitionsPopUp />
+          </Menu.Item>
+          <Menu.Item>
+            <Menu.Header>Trophies</Menu.Header>
+          </Menu.Item>
+        </Menu>
+      </div>
+    )
+  }
 }
 
 export default SideMenu
