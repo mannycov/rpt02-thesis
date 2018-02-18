1a50bb61d92a5920050bf338d2d27a98debb90c3
Emanuel <manny.cov09@gmail.com>
2018-02-17 16:46:39 -0800

    create checkin modal

---
+++ client/src/components/AddCheckIn.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button, Modal } from 'semantic-ui-react'
+
+// Components
+import CheckInForm from './CheckInForm.jsx'
+
+const AddCheckIn = props => (
+  <div>
+    <Button primary onClick={() => { props.show('large') }}>
+      <i className="plus icon" />
+          Check In
+    </Button>
+
+    <Modal open={props.open} onClose={props.close}>
+      <Modal.Header>
+          Enter Check In
+      </Modal.Header>
+      <Modal.Content>
+        <CheckInForm
+          goal={props.goal}
+          close={props.close}
+          handleChange={props.handleChange}
+          handleSubmit={props.handleSubmit}
+        />
+      </Modal.Content>
+    </Modal>
+  </div>
+)
+
+export default AddCheckIn
