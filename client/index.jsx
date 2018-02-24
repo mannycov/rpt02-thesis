d93346b8fff191e6b0e6e2dd6f1e7181d830e111
crizzcoxx <crizzcoxx@gmail.com>
2018-02-24 11:56:53 -0800

    less css changes

--- client/index.jsx
+++ client/index.jsx
@@ -6,8 +6,8 @@ import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import './src/override.css'
-import './src/override.less'
+// import './src/override.css'
+// import './src/override.less'
 
 // import reducers from './reducers'
 
