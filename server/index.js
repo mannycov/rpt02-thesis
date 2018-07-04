90216bb3df658791912766e9ddd33635f4909444
Emanuel <manny.cov09@gmail.com>
2018-07-03 20:51:56 -0700

    edit process env port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import app from './server'
 
 const server = http.createServer(app)
 let currentApp = app
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT, '0.0.0.0' || 3000)
 
 if (module.hot) {
   module.hot.accept('./server', () => {
